09b94241cea35108a704cb9f70e82cf5f562da45
damian <suffodamian02@gmail.com>
2023-08-27 05:51:09 -0300

    background noise effect

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { BackgroundnoiseComponent } from './components/backgroundnoise/backgroundnoise.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    NavbarComponent
+    NavbarComponent,
+    BackgroundnoiseComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/backgroundnoise/backgroundnoise.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-backgroundnoise',
+  templateUrl: './backgroundnoise.component.html',
+  styleUrls: ['./backgroundnoise.component.css']
+})
+export class BackgroundnoiseComponent {
+
+}
