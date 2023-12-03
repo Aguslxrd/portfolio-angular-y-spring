7e3fd79ba2dbe4b7d20a62e6b6a8b5e8cd6639f1
damian <suffodamian02@gmail.com>
2023-12-03 04:00:26 -0300

    cambio de apartado skills y en imgs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,16 @@ import { HomeComponent } from './pages/home/home.component';
 import { AdminComponent } from './pages/admin/admin.component';
 import { LoginComponent } from './pages/login/login.component';
 import { AuthGuard } from './auth.guard';
+import { ProjectscardsComponent } from './components/projectscards/projectscards.component';
+import { SkillsComponent } from './components/skills/skills.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'admin', component: AdminComponent, canActivate: [AuthGuard] }, // Usa el guard aquí
-  { path: '', redirectTo: '/login', pathMatch: 'full' },
-  {path: 'home', component: HomeComponent}
+  //{ path: '', redirectTo: '/login', pathMatch: 'full' },
+  {path: 'home', component: HomeComponent},
+  { path: 'projects', component: ProjectscardsComponent },
+  { path: 'skills', component: SkillsComponent }
 ];
 
 @NgModule({
